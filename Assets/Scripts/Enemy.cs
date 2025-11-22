632b61a5c454b21140089a94c7cec1fab2eb77a4
Amit Ofir <amit.ofir4@gmail.com>
2025-11-22 10:34:31 +0200

    update

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,15 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject destroyedVFX;
     [SerializeField] int hitPoints = 3;
+    [SerializeField] int scoreValue = 10;
+    
+    ScoreBoard scoreBoard;
+
+    private void Start()
+    {
+        scoreBoard = FindFirstObjectByType<ScoreBoard>();
+    }
+
     void OnParticleCollision(GameObject other)
     {
         ProcessHit();
@@ -14,6 +23,7 @@ public class Enemy : MonoBehaviour
         hitPoints--;
         if (hitPoints <= 0)
         {
+            scoreBoard.IncreaseScore(scoreValue);
             Instantiate(destroyedVFX, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
